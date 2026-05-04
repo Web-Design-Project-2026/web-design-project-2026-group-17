cac8aeaf3b7225eb1b44bac4134063ce589362ec
dure25kt <dure25kt@student.ju.se>
2026-05-04 13:26:55 +0200

    java

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// get the cart from localstorage or create an empty array
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+//Function to add a product to the cart
+function addToCart(name,price, image) {
+    const existingProduct = cart.find((item) => item.name === name);
+
+    if (existingProduct) {
+        existingProduct.quantity++;
+    } else {
+        cart.push ({
+            name: name,
+            price: price,
+            image: image,
+            quantity: 1,
+        });
+    }
+
+    //to save the updated cart in localstorage
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    //and to update the cart icon count
+    updateCartCount();
+    alert(name + " has been added!");
+}
+
+//update cart count
+function updateCartCount() {
+    const countElement = document.getElementById("cart-count");
+    if (countElement) {
+        let totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+        countElement.textContent = totalQuantity;
+    }
+}
+
+//the items display on the cart page
+function displayCart () {
+    const cartContainer = document.getElementById("cartItems");
+    const totalElement = document.getElementById("cartTotal");
+
+    if (!cartContainer) return;
+
+    cartContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach((item, index) = {
+        total += item.price * item.quantity; //calculate total price
+
+        cartContainer.innerHTML += `
+        <div class="cart-item">
+        <img scr="${item.image}" width="80">
+        </div>
+        <div>
+        <h3>${item.name}</h3>
+        <p>${item.price} SEK (xs${item.quantity})</p>
+        <button onclick="removeItem(${index})">Delete</button>
+        </div>
+        </div>
+        `;
+    });
+
+    if (totalELement) totalElement.textContent = total;
+}
+
+//to remove an item from the cart
+function removeItem(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    displayCart();
+    updateCartCount();
+}
